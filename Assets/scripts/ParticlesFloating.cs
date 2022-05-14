f3d94e22f44bd0d2acfbbe9fb8fb665878e1a8f4
eugene.kovko <eugene.kovko@tensquaregames.com>
2022-05-14 18:47:08 +0200

    light particles

---
+++ Assets/scripts/ParticlesFloating.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace
+{
+    
+    
+    public class ParticlesFloating : MonoBehaviour
+    {
+        public float timeToMove = 2F;
+        private float timer = 0;
+        private float speed = 0.00003F;
+        private Vector3 newPosition;
+        private void Update()
+        {
+            
+            timer += Time.deltaTime;
+            if (timer > timeToMove)
+            {
+                newPosition = PositionRandomizer.RandomPointInBounds("DreamZone");
+                timer = 0;
+            }
+
+            transform.position = Vector3.Lerp(transform.position, newPosition, speed );
+        }
+
+    }
+}
---
+++ Assets/scripts/ParticlesGenerator.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace DefaultNamespace
+{
+    public class ParticlesGenerator : MonoBehaviour
+    {
+        public int numberOfParticles = 1000;
+        public GameObject prefab;
+        void Start()
+        {
+            SpawnDreams(numberOfParticles);
+        }
+
+        private void SpawnDreams(int i)
+        {
+            while (i > 0)
+            {
+                SpawnDream();
+                i--;
+            }
+        }
+
+
+        private GameObject SpawnDream()
+        {
+            return Instantiate(prefab, PositionRandomizer.RandomPointInBounds("DreamZone"), Quaternion.identity);
+        }
+    }
+}
