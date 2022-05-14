c781a2a9c648e031549ea36ee65012108b2d2eb3
eugene.kovko <eugene.kovko@tensquaregames.com>
2022-05-14 16:59:43 +0200

    Position Randomizer script

--- Assets/scripts/DreamFloating.cs
+++ Assets/scripts/DreamFloating.cs
@@ -16,29 +16,11 @@ namespace DefaultNamespace
             timer += Time.deltaTime;
             if (timer > timeToMove)
             {
-                newPosition = RandomPointInBounds();
+                newPosition = PositionRandomizer.RandomPointInBounds();
                 timer = 0;
             }
 
             transform.position = Vector3.Lerp(transform.position, newPosition, speed );
-        }
-        
-        private Vector3 RandomPosition()
-        {
-            float x = Random.Range(-3F, 3.0F);
-            float y = Random.Range(0.0F, 3.0F);
-            float z = Random.Range(-3F, 3F);
-            return new Vector3(x, y, z);
-        }
-
-        public static Vector3 RandomPointInBounds() {
-            GameObject gameObject = GameObject.Find("DreamZone");
-            Bounds bounds = gameObject.GetComponent<Collider>().bounds;
-            return new Vector3(
-                Random.Range(bounds.min.x, bounds.max.x),
-                Random.Range(bounds.min.y, bounds.max.y),
-                Random.Range(bounds.min.z, bounds.max.z)
-            );
         }
 
     }
---
+++ Assets/scripts/PositionRandomizer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class PositionRandomizer
+    {
+        public static Vector3 RandomPointInBounds() {
+            GameObject gameObject = GameObject.Find("DreamZone");
+            Bounds bounds = gameObject.GetComponent<Collider>().bounds;
+            return new Vector3(
+                Random.Range(bounds.min.x, bounds.max.x),
+                Random.Range(bounds.min.y, bounds.max.y),
+                Random.Range(bounds.min.z, bounds.max.z)
+            );
+        }
+
+    }
+}
