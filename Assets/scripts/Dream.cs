d0c159fa4c7da5c903f12ee431dce54301ab679d
eugene.kovko <eugene.kovko@tensquaregames.com>
2022-05-14 14:22:33 +0200

    new floating script

---
+++ Assets/scripts/Dream.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Serialization;
+using Random = UnityEngine.Random;
+
+namespace DefaultNamespace
+{
+    
+    public class Dream : MonoBehaviour
+    {
+        public int numberOfDreams = 10;
+        public GameObject prefab;
+        void Start()
+        {
+            SpawnDreams(numberOfDreams);
+        }
+
+        private void SpawnDreams(int i)
+        {
+            while (i > 0)
+            {
+                SpawnDream();
+                i--;
+            }
+        }
+
+
+        private GameObject SpawnDream()
+        {
+            return Instantiate(prefab, RandomPosition(), Quaternion.identity);
+        }
+
+        private Vector3 RandomPosition()
+        {
+            float x = Random.Range(-2.0F, 2.0F);
+            float y = 2;
+            float z = Random.Range(-2.0F, 2.0F);
+            return new Vector3(x, y, z);
+        }
+
+    }
+}
---
+++ Assets/scripts/DreamFloating.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class DreamFloating : MonoBehaviour
+    {
+        public float timeToMove = 2F;
+        private float timer = 0;
+        private float speed = 0.005F;
+        private Vector3 newPosition;
+
+        private void Update()
+        {
+            timer += Time.deltaTime;
+            if (timer > timeToMove)
+            {
+                newPosition = RandomPosition();
+                timer = 0;
+            }
+
+            transform.position = Vector3.Lerp(transform.position, newPosition, speed );
+        }
+        
+        private Vector3 RandomPosition()
+        {
+            float x = Random.Range(-3F, 3.0F);
+            float y = Random.Range(0.0F, 3.0F);
+            float z = Random.Range(-3F, 3F);
+            return new Vector3(x, y, z);
+        }
+
+
+    }
+}
